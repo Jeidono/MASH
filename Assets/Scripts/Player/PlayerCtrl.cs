21c9c7fe8a00a3f67a2cf28925e4555cdf10d5c7
Jay <Byrnejay2001@gmail.com>
2025-02-21 19:23:56 +1100

    IsFacing function
    Where character faces left and right added

--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    
     public float movSpeed;
-    float speedX, speedY;
-    Rigidbody2D rb;
+    private float speedX, speedY;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true; // Track player's facing direction
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.linearVelocity = new Vector2(speedX, speedY);
 
+        // Flip the player based on movement direction
+        if (speedX > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (speedX < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight; // Toggle facing direction
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 }
