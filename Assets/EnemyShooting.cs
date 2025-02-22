fdda6d141a2137389d0e8c014ea0f0d16d365403
Jay <Byrnejay2001@gmail.com>
2025-02-22 17:01:20 +1100

    Enemyspidershooting

---
+++ Assets/EnemyShooting.cs
@@ -0,0 +1,28 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletpos;
+    private float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 2){
+            timer = 0;
+            shoot();
+        }
+    }
+    void shoot(){
+        Instantiate(bullet, bulletpos.position, quaternion.identity);
+    }
+}
