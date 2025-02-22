e056390927be182a397a07a04979d37ad2807b7c
Jay <Byrnejay2001@gmail.com>
2025-02-22 18:29:33 +1100

    update

--- Assets/Scripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyBulletScript.cs
@@ -8,8 +8,6 @@ public class EnemyBulletScript : MonoBehaviour
     public float force;
     private float timer;
 
-    public Health health;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
