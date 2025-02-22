6c341de06014d20ac424c7615e29b519253ff4f7
Jay <Byrnejay2001@gmail.com>
2025-02-22 15:56:24 +1100

    Game functionality

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverUi;
+    public GameObject winScreenUi;
+
     void Start()
     {
         Cursor.visible = false;
@@ -13,41 +16,58 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameOverUi.activeInHierarchy)
+        // Check if either the gameOver or win screen is active
+        if (gameOverUi.activeInHierarchy || winScreenUi.activeInHierarchy)
         {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
         else
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-        if (Input.GetKeyDown(KeyCode.R)){
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             restart();
         }
-        if (Input.GetKeyDown(KeyCode.Q)){
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             mainMenu();
         }
     }
-    
+
     public void gameOver()
     {
         gameOverUi.SetActive(true);
     }
+
+    public void WinScreen()
+    {
+        winScreenUi.SetActive(true);
+    }
+
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    
-    public void mainMenu(){
+
+    public void mainMenu()
+    {
         SceneManager.LoadScene("MainMenu");
-        {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
-        }
     }
-    public void Quit(){
-       Application.Quit();
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    internal void WinScreenUi()
+    {
+        throw new NotImplementedException();
     }
 }
--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -1,15 +1,31 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerCtrl : MonoBehaviour
 {
     public float movSpeed;
     private float speedX, speedY;
     private Rigidbody2D rb;
-    private bool isFacingRight = true; // Track player's facing direction
+    private bool isFacingRight = true; 
+
+    private int honeyCounter = 0;
+    private int honeyStorage = 0;
+    private int totalHoneyCollected = 0;
+    private const int maxCarry = 3;
+    private const int winCondition = 1;
+    private bool isInBase = false;
+
+    public TMP_Text counterText;
+    public TMP_Text totalHoneyText;
+    public GameObject Uihoney1, Uihoney2, Uihoney3;
+    public GameManager gameManager;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        UpdateUI();
     }
 
     void Update()
@@ -18,7 +34,6 @@ public class PlayerCtrl : MonoBehaviour
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.linearVelocity = new Vector2(speedX, speedY);
 
-        // Flip the player based on movement direction
         if (speedX > 0 && !isFacingRight)
         {
             Flip();
@@ -27,11 +42,68 @@ public class PlayerCtrl : MonoBehaviour
         {
             Flip();
         }
+
+        if (isInBase && honeyCounter > 0 && Input.GetKeyDown(KeyCode.E))
+        {
+            DepositHoney();
+        }
     }
 
     void Flip()
     {
-        isFacingRight = !isFacingRight; // Toggle facing direction
+        isFacingRight = !isFacingRight;
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Honey") && collision.gameObject.activeSelf && honeyCounter < maxCarry)
+        {
+            collision.gameObject.SetActive(false);
+            honeyCounter++;
+            UpdateUI();
+        }
+        else if (collision.CompareTag("Beehive1") || collision.CompareTag("Beehive2"))
+        {
+            isInBase = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Beehive1") || collision.CompareTag("Beehive2"))
+        {
+            isInBase = false;
+        }
+    }
+
+    private void DepositHoney()
+    {
+        honeyStorage += honeyCounter;
+        totalHoneyCollected += honeyCounter;
+        honeyCounter = 0;
+        UpdateUI();
+        CheckWinCondition();
+    }
+
+    void UpdateUI()
+    {
+        Uihoney1.SetActive(honeyCounter >= 1);
+        Uihoney2.SetActive(honeyCounter >= 2);
+        Uihoney3.SetActive(honeyCounter >= 3);
+        counterText.text = "Storage: " + honeyStorage;
+        totalHoneyText.text = "Total Honey: " + totalHoneyCollected;
+    }
+
+    void CheckWinCondition()
+    {
+        if (honeyStorage >= winCondition)
+        {
+    
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            gameManager.WinScreen();
+            Debug.Log("You Win!");
+        }
     }
 }
