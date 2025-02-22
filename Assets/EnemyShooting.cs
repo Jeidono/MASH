1fccc931f013644e056a883781e9185e8065224e
Jay <Byrnejay2001@gmail.com>
2025-02-22 17:41:38 +1100

    shooting update

--- Assets/EnemyShooting.cs
+++ Assets/EnemyShooting.cs
@@ -6,22 +6,30 @@ public class EnemyShooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletpos;
     private float timer;
+    private GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if(timer > 2){
+         if(distance <7)
+         {
+            timer += Time.deltaTime;
+
+             if(timer > 2){
             timer = 0;
             shoot();
         }
     }
+         }
+       
     void shoot(){
         Instantiate(bullet, bulletpos.position, quaternion.identity);
     }
