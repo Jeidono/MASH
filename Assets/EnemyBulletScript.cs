d7c6071c759dcfb484e826c1cb9f1794e709d446
Jay <Byrnejay2001@gmail.com>
2025-02-22 18:03:17 +1100

    take damage from spiders

--- Assets/EnemyBulletScript.cs
+++ Assets/EnemyBulletScript.cs
@@ -7,23 +7,44 @@ public class EnemyBulletScript : MonoBehaviour
     private Rigidbody2D rb;
     public float force;
     private float timer;
+
+    public Health health;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
-        Vector3 direction = player.transform.position - transform.position;
-        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+        
+        {
+            Vector3 direction = player.transform.position - transform.position;
+            rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
 
-        float rot = Mathf.Atan2(-direction.x, -direction.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+            float rot = Mathf.Atan2(-direction.x, -direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         timer += Time.deltaTime;
-        if(timer >5){
+        if(timer > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           
+            Health playerHealth = other.gameObject.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1);  
+            }
             Destroy(gameObject);
         }
     }
